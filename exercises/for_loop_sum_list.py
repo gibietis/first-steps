85448a1e3d3f3962ed5ea006088416d7dd94f82d
Igor <igorcardosokaize@gmail.com>
2022-07-28 21:02:23 -0300

    New exercises added

---
+++ exercises/for_loop_sum_list.py
@@ -0,0 +1,8 @@
+#Create a function “add” that receives a list as parameter and returns the sum of all
+#elements in the list. Use the “for” loop to iterate over the elements of the list.
+lista = [10, 20, 30, 40, 50]
+s_lista = 0
+for x in lista:
+    s_lista = s_lista + x
+    print(s_lista)
+
---
+++ exercises/x = 6666.py
@@ -0,0 +1,6 @@
+x = 6666
+y = 0
+while resultado < 99999999
+    x = x + x
+    y = y + 1
+    print(resultado)
