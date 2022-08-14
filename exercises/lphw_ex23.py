6ec992ccbfa45951d5ae0bd78293c134459280d2
Igor <igorcardosokaize@gmail.com>
2022-08-14 09:51:03 -0300

    Changes made to a file

--- exercises/lphw_ex23.py
+++ exercises/lphw_ex23.py
@@ -1,6 +1,7 @@
 import sys
 script, encoding, error = sys.argv
 
+
 def main(language_file, encoding, errors):
     line = language_file.readline()
 
@@ -8,12 +9,14 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main (language_file, encoding, errors)
 
+
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
+
 
 languages = open("languages.txt", encoding="utf-8")
 
