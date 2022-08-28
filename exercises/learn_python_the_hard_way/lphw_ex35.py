41266270573a3f9cbb96f4c4ea3873c8b352c6ed
Igor <igorcardosokaize@gmail.com>
2022-08-27 23:40:31 -0300

    Added exercises

--- exercises/learn_python_the_hard_way/lphw_ex35.py
+++ exercises/learn_python_the_hard_way/lphw_ex35.py
@@ -4,6 +4,7 @@ def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice = input("> ")
+
     if "0" in choice or "1" in choice:
         how_much = int(choice)
     else:
